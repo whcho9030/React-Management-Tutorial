c21ca8049d61084f009be02967c9c7217ff19ffd
whcho9030 <whcho9030@gmail.com>
2020-09-09 16:26:47 +0900

    lecture 5

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '홍길동',
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '나동빈',
   'birthday': '961222',
   'gender': '남자',
   'job': '대학생'
-};
+}, {
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '의적'
+}, {
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '조원희',
+  'birthday': '900325',
+  'gender': '남자',
+  'job': '프로그래머'
+}];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
     );
   }
 }
